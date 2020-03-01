9623d3efef2248da0316f0b8fe0a8e75ff0da431
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-03-01 08:47:21 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,8 +5,25 @@
 # }
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each do |name, age|
+    
+ birthday_kids.each do |name, age| 
     puts "Happy Birthday #{name}! You are now #{age} years old!"
+   
+ 
+
+    end
+ end
+ 
+ def age_appropriate_birthday(birthday_kids)
+ if block_given?
+  i = 0 
+  
+  while i < birthday_kids.length
+  yield birthday_kids[i]
+      i = i + 1 
+    end
+    birthday_kids
+  else
+    puts "You are too old for this."
   end
 end
-
